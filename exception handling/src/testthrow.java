fccea991c5e96f9c8716c55ca25d6401d4def037
potthuri-charan <potthuricharanpadmasrikhar@gmail.com>
2023-02-13 10:05:19 +0530

    Create testthrow.java

---
+++ exception handling/src/testthrow.java
@@ -0,0 +1,15 @@
+public class testthrow {
+    public static void validate(int age) {
+        if(age<18){
+            throw new ArithmeticException("Person is not eligible to vote");
+        }
+        else{
+            System.out.println("Person is eligible to vote!!");
+        }
+    }
+
+    public static void main(String[] args) {
+        validate(19);
+        System.out.println("Rest of the code.....");
+    }
+}
